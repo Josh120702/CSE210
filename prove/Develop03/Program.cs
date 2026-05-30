87a6170db1c166d2d4fc7294101f37374f98b678
Josh120702 <jcyoung1207@gmail.com>
2026-05-30 17:32:58 -0600

    scripture program

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Reference reference = new Reference("Proverbs",3, 5, 6);
+
+        Scripture scripture = new Scripture(
+            reference,
+            "Trust in the Lord with all thine heart and lean not unto thine own understanding In all thy ways acknowledge him and he shall direct thy paths"
+        );
+
+        while (!scripture.AllHidden())
+        {
+            Console.Clear();
+            scripture.Display();
+
+            Console.WriteLine();
+            Console.Write("Press Enter to continue or type quit: ");
+            string input = Console.ReadLine();
+            if (input.ToLower() == "quit")
+            {
+                return;
+            }
+            scripture.HideRandomWords(3);
+        }
+        Console.Clear();
+        scripture.Display();
+        Console.WriteLine("\nAll words are hidden.");
     }
 }
---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,26 @@
+public class Word
+{
+    private string _word;
+    private bool _hidden;
+    public Word(string word)
+    {
+        _word = word;
+        _hidden = false;
+    }
+    public void Hide()
+    {
+        _hidden = true;
+    }
+    public bool IsHidden()
+    {
+        return _hidden;
+    }
+    public string Display()
+    {
+        if (_hidden)
+        {
+            return new string('_', _word.Length);
+        }
+        return _word;
+    }
+}
